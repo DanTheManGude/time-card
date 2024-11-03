640875ec17e8c3009ab5385418f3260c1beca654
DanTheManGude <dgude31@gmail.com>
2024-11-03 16:09:56 -0700

    no accordians

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,15 +1,4 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-import {
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Stack,
-  Typography,
-} from "@mui/material";
-import { ExpandMore } from "@mui/icons-material";
+import { Stack } from "@mui/material";
 
 import { withDays } from "./WithDays";
 
@@ -17,31 +6,12 @@ function renderDay(day: Day) {
   return day.date.toLocaleString();
 }
 
-function renderDays(days: Day[], isPast: boolean) {
-  return (
-    <Accordion defaultExpanded={!isPast}>
-      <AccordionSummary expandIcon={<ExpandMore />} id={`renderDays-${isPast}`}>
-        <Typography variant="h6">
-          {isPast ? "Past Days" : "Current and Future Days"}
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Stack direction={"column"}>{days.map(renderDay)}</Stack>
-      </AccordionDetails>
-    </Accordion>
-  );
-}
-
 function Title(props: WithDaysProps) {
   const { days } = props;
 
-  const [pastDays, setPastDays] = useState<Day[]>([]);
-  const [presentAndFutureDays, setPresentAndFutureDays] = useState<Day[]>([]);
-
   return (
     <Stack direction={"column"} width={"100%"} paddingY={3} spacing={2}>
-      {renderDays(pastDays, true)}
-      {renderDays(presentAndFutureDays, false)}
+      {days.map(renderDay)}
     </Stack>
   );
 }
