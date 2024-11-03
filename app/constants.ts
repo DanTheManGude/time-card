22181f30a15855c1d08498d01856ed06ca481822
DanTheManGude <dgude31@gmail.com>
2024-11-03 11:55:16 -0700

    setup mui

---
+++ app/constants.ts
@@ -0,0 +1,8 @@
+export const fontFamilies = [
+  "Book Antiqua",
+  "Palatino",
+  "Palatino Linotype",
+  "Palatino LT STD",
+  "Georgia",
+  "serif",
+];
---
+++ app/theme.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+import { fontFamilies } from "./constants";
+
+export default createTheme({
+  typography: {
+    fontFamily: fontFamilies.join(","),
+  },
+});
