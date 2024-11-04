1123c928e4099e4a40da8041208f2a9a38cff553
DanTheManGude <dgude31@gmail.com>
2024-11-03 23:11:00 -0700

    getFirstAndLastDays

--- app/utility.ts
+++ app/utility.ts
@@ -1,8 +1,49 @@
 const LOCAL_STORAGE_KEY = "TIME_CARD_PAY_PERIOD";
 
+function isWeekday(date: Date) {
+  const dayOfWeek = date.getDay();
+
+  return dayOfWeek === 0 || dayOfWeek === 6;
+}
+
+function getFirstAndLastDays() {
+  var firstDate;
+  var lastDate;
+
+  const today = new Date();
+
+  if (today.getDate() <= 15) {
+    firstDate = new Date(today.getFullYear(), today.getMonth(), 1);
+    lastDate = new Date(today.getFullYear(), today.getMonth(), 15);
+  } else {
+    const lastWeekdayOfMonth = new Date(
+      today.getFullYear(),
+      today.getMonth() + 1,
+      0
+    );
+
+    while (!isWeekday(lastWeekdayOfMonth)) {
+      lastWeekdayOfMonth.setDate(lastWeekdayOfMonth.getDate() - 1);
+    }
+
+    if (today.getDate() <= lastWeekdayOfMonth.getDate()) {
+      firstDate = firstDate = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        16
+      );
+      lastDate = lastWeekdayOfMonth;
+    } else {
+      firstDate = new Date(today.getFullYear(), today.getMonth() + 1, 1);
+      lastDate = new Date(today.getFullYear(), today.getMonth() + 1, 15);
+    }
+  }
+
+  return { firstDate, lastDate };
+}
+
 function constructNewPayPeriod(): PayPeriod {
-  //TODO
-  return { days: [], lastDate: new Date() };
+  const { firstDate, lastDate } = getFirstAndLastDays();
 }
 
 export function savePayPeriod(payPeriod: PayPeriod) {
