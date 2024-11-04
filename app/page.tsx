a29e2273415893668985b3dcb120d4c59c15500b
DanTheManGude <dgude31@gmail.com>
2024-11-04 11:24:08 -0700

    hack remove

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import { Container, Typography } from "@mui/material";
 
-import Body from "./components/main";
-
 export default function Page() {
   return (
     <Container sx={{ paddingTop: 2 }}>
       <Typography variant="h3" textAlign={"center"}>
         Time Card
       </Typography>
-      <Body />
     </Container>
   );
 }
