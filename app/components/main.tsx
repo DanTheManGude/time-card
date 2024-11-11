b4bf3c317c955a5a1f8e96dd9fd91a4718dc0839
DanTheManGude <dgude31@gmail.com>
2024-11-10 18:40:55 -0700

    quarterHourDifference title

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import withPayPeriod from "./withPayPeriod";
 import DayRow from "./dayRow";
 import { useCallback } from "react";
+import { convertQuarterHoursToString } from "../utility";
 
 function Main(props: WithPayPeriodProps) {
   const {
-    payPeriod: { days },
+    payPeriod: { days, quarterHourDifference },
     updateDay,
   } = props;
 
@@ -23,6 +24,13 @@ function Main(props: WithPayPeriodProps) {
 
   return (
     <Stack direction={"column"} width={"100%"} paddingY={3} spacing={2}>
+      {quarterHourDifference && (
+        <Typography>
+          {`You are ${
+            quarterHourDifference > 0 ? "ahead" : "behind"
+          } by ${convertQuarterHoursToString(Math.abs(quarterHourDifference))}`}
+        </Typography>
+      )}
       {days.map((day, index) => (
         <DayRow
           day={day}
