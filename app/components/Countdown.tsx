59e58d1d344a5bb26e60ccc708a2a72bb753a777
DanTheManGude <dgude31@gmail.com>
2025-11-23 10:29:26 -0700

    color around countdown

--- app/components/Countdown.tsx
+++ app/components/Countdown.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { useMemo } from "react";
 
 const lastDay = new Date("2026-08-05T17:00:00-07:00");
@@ -16,8 +16,21 @@ export default function Countdown() {
   }, [todaysDate]);
 
   return (
-    <Typography textAlign="center" variant="h5" gutterBottom>
-      {message}
-    </Typography>
+    <Stack
+      direction={"row"}
+      justifyContent={"center"}
+      alignContent={"space-around"}
+    >
+      <Typography
+        textAlign="center"
+        variant="h5"
+        gutterBottom
+        bgcolor={"primary.main"}
+        color={"primary.contrastText"}
+        paddingInline={1}
+      >
+        {message}
+      </Typography>
+    </Stack>
   );
 }
