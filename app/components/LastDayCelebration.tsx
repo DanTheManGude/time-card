cd8ef8e08b8562190e75d5c35471950c973492a4
DanTheManGude <dgude31@gmail.com>
2026-08-24 23:19:14 -0600

    use video

--- app/components/LastDayCelebration.tsx
+++ app/components/LastDayCelebration.tsx
@@ -1,33 +1,37 @@
 import { Box, Stack, Typography } from "@mui/material";
-import PartyGif from "../assets/party.gif";
 
 export default function LastDayCelebration() {
   return (
-    <Stack
-      direction={"column"}
-      justifyContent={"center"}
-      alignContent={"space-around"}
-      pt={5}
-    >
-      <Typography
-        textAlign="center"
-        variant="h5"
-        gutterBottom
-        color={"primary"}
-        paddingInline={1}
+    <>
+      <Stack
+        direction={"column"}
+        justifyContent={"center"}
+        alignContent={"space-around"}
+        pt={5}
       >
-        {"Congratualations on making it to the end :)"}
-      </Typography>
-      <Box
-        component="img"
-        src={PartyGif.src}
-        alt="party"
-        sx={{
-          width: "100%",
-          maxWidth: 400,
-          height: "auto",
-        }}
-      />
-    </Stack>
+        <Typography
+          textAlign="center"
+          variant="h5"
+          gutterBottom
+          color={"primary"}
+          paddingInline={1}
+        >
+          {"Congratualations on making it to the end :)"}
+        </Typography>
+        <Box
+          component="video"
+          src={"/dog.mp4"}
+          autoPlay
+          loop
+          muted
+          playsInline
+          sx={{
+            width: "100%",
+            height: "auto",
+            borderRadius: 2,
+          }}
+        />
+      </Stack>
+    </>
   );
 }
